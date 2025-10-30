5ce4f65d5b1aed1d1673ba68cd295d7a89113e31
Burnt-Waffle <michael568@hanmail.net>
2025-10-30 15:11:59 +0900

    refactor: Merge WebFilterConfig to WebConfig

--- src/main/java/com/mika/ktdcloud/community/config/WebConfig.java
+++ src/main/java/com/mika/ktdcloud/community/config/WebConfig.java
@@ -1,10 +1,16 @@
 package com.mika.ktdcloud.community.config;
 
+import com.mika.ktdcloud.community.jwt.JwtAuthFilter;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -15,5 +21,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public FilterRegistrationBean<Filter> jwtFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(jwtAuthFilter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
     }
 }
