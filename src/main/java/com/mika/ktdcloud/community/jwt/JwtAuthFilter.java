0ab64149c741f0f8088c7dc37c15bfad41971c97
Burnt-Waffle <michael568@hanmail.net>
2025-12-07 00:05:10 +0900

    fix: fix typo

--- src/main/java/com/mika/ktdcloud/community/jwt/JwtAuthFilter.java
+++ src/main/java/com/mika/ktdcloud/community/jwt/JwtAuthFilter.java
@@ -29,7 +29,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             "/api/v1/auth/login",
             "/api/v1/auth/refresh",
             "/api/v1/auth/logout",
-            "/api/v1//terms"
+            "/api/v1/terms"
     };
 
     @Override
